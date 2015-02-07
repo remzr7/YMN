d21db91e887fffe86760adf49fa154abc45a3f37
remzr7 <remzr7@gmail.com>
2015-02-07 14:27:21 +0800

    Initial Commit

---
+++ NomNom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NomNom
+//
+//  Created by Rameez Remsudeen  on 2/7/15.
+//  Copyright (c) 2015 Rameez Remsudeen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NomNom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NomNom
+//
+//  Created by Rameez Remsudeen  on 2/7/15.
+//  Copyright (c) 2015 Rameez Remsudeen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
